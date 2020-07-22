5285bcc1051a5ca831d12d38d38503feb22817e9
19047027 <19047027@DESKTOP-HTRBJHT>
2020-07-22 13:26:19 +0800

    retrieveAllChromebookTest()

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -79,7 +79,7 @@ public class ResourceCentreTest {
 		assertEquals("Check that ViewAllCamcorderlist", testOutput, allCamcorder);
 		
 	}
-	//Changes by kaiqi
+	//Changes by Kaiqi
 	@Test
 	public void retrieveAllChromebookTest() {
 		//fail("Not yet implemented");
